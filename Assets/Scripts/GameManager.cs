c4aedba44890507cc9f010f0fdc7c0183c5270ea
Lepi009 <leander.pieters@web.de>
2021-05-15 13:47:35 +0200

    Aufgabe implentiert

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 ﻿using i5.Toolkit.Core.Spawners;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float spawnInterval = 5f;
     [SerializeField] Spawner spawner;
+
+    [SerializeField] TMP_Text labelText;
+    [SerializeField] GameObject playButton;
+
     float remainingTime;
+    int targetCount = 10;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +25,19 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasStarted) return;
+
         remainingTime -= Time.deltaTime;
         if(remainingTime <= 0)
         {
             remainingTime = spawnInterval;
-            SpawnTarget();
+            if(targetCount <= 0)
+            {
+                EndGame();
+            } else
+            {
+                SpawnTarget();
+            }
         }
     }
 
@@ -32,6 +47,24 @@ public class GameManager : MonoBehaviour
         {
             GameObject target = spawner.SpawnedInstances[0];
             target.transform.position = new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-1.5f, 1.5f), 2);
+            targetCount--;
+            labelText.text = targetCount + " targets remaining...";
         }
+    }
+
+    public void StartGame()
+    {
+        hasStarted = true;
+        labelText.text = "Get ready!";
+        playButton.SetActive(false);
+    }
+
+    public void EndGame()
+    {
+        playButton.SetActive(true);
+        targetCount = 10;
+        labelText.text = "Press play!";
+        Destroy(spawner.MostRecentlySpawnedObject);
+        hasStarted = false;
     }
 }
